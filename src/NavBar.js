62be9f71051df7e28b37250b3ea4fdf5c31d2413
rohith4pr <rohith4pr@gmail.com>
2022-05-04 23:17:53 +0530

    second-comit

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,12 +1,13 @@
 import React from 'react';   
 import logo from './logo.svg';
+import menu from './menu.png';
 
 const NavBar = () => (
     <nav>
         <ul>
             <li className='logo'> 
-                <img alt='some value' src={logo}/> 
-                <div className='amritha'>AMRITHA</div>
+                <img width='60px' alt='some value' src={logo}/> 
+                <div className='amritha'>AMRITA</div>
                 <div className='amrithaINNER'>ANALYTICS LAB</div>
             </li>
             <li className='right'>
@@ -23,6 +24,9 @@ const NavBar = () => (
             </li>
             <li style={{marginRight : '70px'}} className='right'>
                 LOGIN
+            </li>
+            <li>
+            <img className='hammenu' style={{marginTop : '5px'}} width='42px' height='50px' alt='some value' src={menu}/> 
             </li>
         </ul>
     </nav>
--- src/components/HomeMain.js
+++ src/components/HomeMain.js
@@ -2,19 +2,26 @@ import man from '../man.svg';
 
 const HomeMain = () => (
     <div className='sidebyside'>
+        <div className='swapfalse' style={{flexBasis: '50%', paddingLeft:'50px', paddingTop:'50px'}}>
+            <img style={{
+                        width: '300px',
+                        height: '400px'
+                    }} alt='nothing' src={man}/>
+        </div>
         <div style={{flexBasis: '50%',marginLeft:'60px', paddingLeft:'50px',paddingRight:'20px',paddingTop:'100px',margin:'10px'}}>
             <div style={{textAlign: 'right'}} className="header">
+     
                 Are You Looking for Analytical Solutions for Your Business?
             </div>
             <div style={{paddingTop:'25px',textAlign: 'right'}} className="headerright">
                 We are a group of proactive analytics enthusiasts, who work tirelessly to solve data problems and exchange ideas of how to improve business using analytics.
             </div>
         </div>
-        <div style={{flexBasis: '50%', paddingLeft:'50px', paddingTop:'50px'}}>
+        <div className='swaptrue' style={{flexBasis: '50%', paddingLeft:'50px', paddingTop:'50px'}}>
             <img style={{
                         width: '300px',
                         height: '400px'
-                    }} alt='some value' src={man}/>
+                    }} alt='nothing' src={man}/>
         </div>
     </div>
 );
