4e4ce7ff96de62336ac22726b6e27f9374cc6be3
akshay <akshaymohan99@gmail.com>
2022-05-05 16:19:53 +0530

    Merge branch 'main' of https://github.com/rohith4pr/Amrita-Analytics-Lab into main

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -1,8 +1,37 @@
-const BlogPage = () => (
-    <div className="Home">
-        Hello
-    </div>
-)
+const BlogPage = () => {
+    const RecentPostInfo = () =>{
+
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+    return(
+        <div className="Blog">
+            <div className="BlogHeader">
+                Blog
+            </div>
+            <div className="BlogContent">
+                <div style={{flexBasis : "30%"}}>
+                    <div className="BlogRecentHeader">
+                        Recents
+                    </div>
+                    <RecentPostInfo/>
+                </div>
+                <div style={{flexBasis : "40%",background:'white'}}>
+                    Recents
+                </div>
+                <div style={{flexBasis : "30%"}}>
+                    Recents
+                </div>
+                
+            </div>
+        </div>
+    );
+}
+    
+
 
 
 export default BlogPage;
