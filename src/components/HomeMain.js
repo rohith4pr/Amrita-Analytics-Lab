d4fd0f2c8b614fe1ace96f920e74d73a984b5a6c
cpsanjay <cpsanjay1999@gmail.com>
2022-05-05 12:19:38 +0530

    sanju

--- src/components/HomeMain.js
+++ src/components/HomeMain.js
@@ -2,7 +2,7 @@ import man from '../man.svg';
 
 const HomeMain = () => (
     <div className='sidebyside'>
-        <div className='swapfalse' style={{flexBasis: '50%', paddingLeft:'50px', paddingTop:'50px'}}>
+        <div className='swapfalse' style={{flexBasis: '60%', paddingLeft:'50px', paddingTop:'50px'}}>
             <img style={{
                         width: '300px',
                         height: '400px'
@@ -13,11 +13,11 @@ const HomeMain = () => (
      
                 Are You Looking for Analytical Solutions for Your Business?
             </div>
-            <div style={{paddingTop:'25px',textAlign: 'right'}} className="headerright">
+            <div style={{paddingTop:'25px',textAlign: 'right', paddingLeft: '50px'}} className="headerright">
                 We are a group of proactive analytics enthusiasts, who work tirelessly to solve data problems and exchange ideas of how to improve business using analytics.
             </div>
         </div>
-        <div className='swaptrue' style={{flexBasis: '50%', paddingLeft:'50px', paddingTop:'50px'}}>
+        <div className='swaptrue' style={{flexBasis: '40%', paddingLeft:'50px', paddingTop:'50px'}}>
             <img style={{
                         width: '300px',
                         height: '400px'
