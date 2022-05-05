b9b9d9bf465b45c489551960ecf1d19131e9f891
raedmajeed <100485450+raedmajeed@users.noreply.github.com>
2022-05-05 16:32:54 +0530

    service updated

--- src/pages/ServicesPage.js
+++ src/pages/ServicesPage.js
@@ -1,5 +1,7 @@
 import '../service.css';
-<<<<<<< HEAD
+import NavBar from '../NavBar';
+import Footer from '../Footer';
+
 import { useState } from "react";
 
 const ServicesPage = () => {
@@ -12,81 +14,21 @@ const ServicesPage = () => {
 
     return(
         <div className="hero">
+            <div>
+              <NavBar />
+            </div>
             <h1>Services</h1>
             <div className="service-nav-container">
                 <div><button className={toggleState === 1? "sevice-nav-ele-mod" : "service-nav-ele"} onClick={() => optionClick(1)} >Consulting</button></div>
                 <div><button className={toggleState === 2? "sevice-nav-ele-mod" : "service-nav-ele"} onClick={() => optionClick(2)} >Learning</button></div>
                 <div><button className={toggleState === 3? "sevice-nav-ele-mod" : "service-nav-ele"} onClick={() => optionClick(3)} >Courses</button></div>
                 <div><button className={toggleState === 4? "sevice-nav-ele-mod" : "service-nav-ele"} onClick={() => optionClick(4)} >Internship</button></div>
-            </div>    
+            </div> 
+            <div>
+              <Footer />
+            </div>   
         </div>
     )
 }
-
-=======
-import NavBar from '../NavBar';
-
-const ServicesPage = () => (
-    <div className="Home">
-      <div>
-          <NavBar />
-      </div>
-      <div className="main">
-      <h1>Services</h1>
-      <div className="service-dev">
-        <div><button onclick="animation()">Consulting</button></div>
-        <div><button className="service-nav" data-for-tab="2">Learning</button></div>
-        <div><button className="service-nav" data-for-tab="3">Courses</button></div>
-        <div> <button className="service-nav" data-for-tab="4">Internship</button></div>
-      </div>
-    </div>
-    <div className="container-content" data-tab="1">
-      <div className="test">
-        <ul>
-        <li className="list">We offer internship opportunities for graduate and post-graduate students</li>
-        </ul>
-      </div>
-    </div>
-    <div className="container-content" data-tab="2">
-      <div className="test"><ul>
-        
-        <li className="list">Data Analysis using</li>
-        <li className="list" >Data Analysis using</li>
-        <li className="list">Data Analysis using Julia</li>
-      </ul></div>
-      <div className="test"><ul>
-        <li className="list">Data visualization using Python</li>
-        <li className="list">Data visualization using R</li>
-        <li className="list">Data visualization using Python</li>
-        <li className="list">Data visualization using Tableau</li>
-        <li className="list">Data visualization using Power BI</li>
-      </ul></div>
-      <div className="test"><ul>
-        <li className="list">Data Analysis using</li>
-        <li className="list" >Data Analysis using</li>
-        <li className="list">Data Analysis using Julia</li>
-      </ul></div>
-      <div className="test"><ul>
-        <li className="list">Data visualization using R</li>
-        <li className="list">Data visualization using Python</li>
-        <li className="list">Data visualization using Python</li>
-        <li className="list">Data visualization using Tableau</li>
-        <li className="list">Data visualization using Power BI</li>
-      </ul></div>
-      <div className="test">
-        <ul>
-          <li className="list">Advanced 
-            Multivariate Data</li>
-          <li className="list">Time Series Analysis and 
-            Mac<div className="container-new">hine Learning</div></li>
-          <li className="list">Deep 
-            Natural Language Processing</li>
-        </ul>
-      </div>
-    </div>
-    </div>
-)
->>>>>>> f572067bd82fbdfeafe84f475e1d16b6b5e4d416
-
 
 export default ServicesPage;
