2c2dbef0aa60519c0aac62dc88b1dcf12db51d48
cpsanjay <cpsanjay1999@gmail.com>
2022-05-05 13:55:04 +0530

    sanju

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,8 +1,21 @@
 import React from "react";
+import Page404 from "../404.svg"
 
 const NotFoundPage = () => (
-    <div className="Home">
-    <h1> 404 : Page Not Found </h1>
+    <div className="NotFound">
+        <img style={{flexBasis: "50%", width:'640px'}} src = {Page404}></img>
+        <div className="MainText404">
+        Sorry,
+            <div>
+            Page Not Found
+            </div>
+            <div className="Woops">
+            Wooops! It looks like you are lost
+            </div>
+            <div className="BackHome">
+            Back Home
+            </div>
+        </div>
     </div>
 );
 
