3849a2ee9628dedea0c136281cdce4df96b59be4
raedmajeed <100485450+raedmajeed@users.noreply.github.com>
2022-05-11 12:19:30 +0530

    signup page updated

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -29,27 +29,13 @@ const LoginPage = () => {
           <h2>Login your account</h2>
           <input style={{borderBottom: '1px solid #FFFFFF'}} type="email" className="FormField" placeholder="USERNAME" name="name" id='name' required />
           <input style={{borderBottom: '1px solid #FFFFFF', marginTop : '70px'}}type="password" className="FormField" placeholder="PASSWORD" name="name" id='name' required />
-<<<<<<< HEAD
-          {/* <div style={{borderBottom: '1px solid #FFFFFF',marginLeft: '40px' , marginRight : '40px'}}>
-          </div> */}
-          <div style={{display:'flex'}}>
-              <div className='buttonstuffLogin'>
-                LOGIN
-              </div>
-              <div style={{marginLeft:'200px'}} className='buttonstuffLogin'>
-                <NavLink style={{color: 'inherit', textDecoration: 'inherit'}} to="/signup">
-                  SIGNUP
-                </NavLink>
-              </div>
-=======
           <div className='buttonstuffLogin'>
             LOGIN
->>>>>>> d1737cca72598a3ac08d41eb2f5eae373ff7a064
           </div>
         </div>
         <div className="LoginRight">
           <div>
-            <img width='200px' alt='some value' src={logoLogin}/> 
+            <img width='300px' alt='some value' src={logoLogin}/> 
           </div>
           <div>
             <div className='amrithaLogin'>AMRITA</div>
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -3,6 +3,7 @@ import { NavLink } from 'react-router-dom';
 import { useEffect } from 'react';
 import './signup.css';
 import logo from '../logo.svg';
+import logoLogin from '../logoLogin.svg';
 
 const SignUp = ({user}) =>{
   useEffect(() => {
@@ -24,8 +25,29 @@ const SignUp = ({user}) =>{
         <div className='hero-container'>
             <div className='hero'>
               <div>
-                <p style={{font:'montserrat', fontWeight:'bolder', fontSize:'30px', padding:'10px 20px'}}>SignUp</p>
+                <p style={{font:'montserrat', fontWeight:'bolder', fontSize:'50px', padding:'10px 40px',color:'white'}}>SignUp</p>
               </div>
+              <div style={{flex:'flex'}}>
+                    <input style={{borderBottom: '1px solid #FFFFFF',marginBottom:'50px'}} type="email" className="FormField" placeholder="FIRST NAME" name="name" id='name' required />
+                    <input style={{borderBottom: '1px solid #FFFFFF'}} type="email" className="FormField" placeholder="LAST NAME" name="name" id='name' required />
+              </div>
+              <input style={{borderBottom: '1px solid #FFFFFF'}} type="email" className="FormField" placeholder="USERNAME" name="name" id='name' required />
+              <input style={{borderBottom: '1px solid #FFFFFF', marginTop : '70px'}}type="password" className="FormField" placeholder="PASSWORD" name="name" id='name' required />
+              <input style={{borderBottom: '1px solid #FFFFFF', marginTop : '70px'}}type="password" className="FormField" placeholder="CONFIIRM PASSWORD" name="name" id='name' required />
+            
+              <div className='buttonstuffLogin'>
+                  SIGN UP
+              </div>
+             
+            </div>
+            <div style={{display:'flex',flexDirection:'column',marginLeft:'120px',marginTop:'80px'}}>
+                <div style={{marginLeft:'30px'}}>
+                    <img width='200px' alt='some value' src={logoLogin}/> 
+                </div>
+                <div>
+                    <div className='amrithaLogin'>AMRITA</div>
+                    <div className='amrithaINNERLogin'>ANALYTICS LAB</div>
+                </div>
             </div>
         </div>
     </div>
