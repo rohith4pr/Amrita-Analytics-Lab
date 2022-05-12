4df2d88adda6df1274e30090525beab8acfa8b31
cpsanjay <cpsanjay1999@gmail.com>
2022-05-13 01:10:11 +0530

    addBlog changes

--- src/pages/AddBlog.js
+++ src/pages/AddBlog.js
@@ -14,7 +14,7 @@ const AddBlog = ({user,setUser}) => {
                 </div>
                 <div style={{display:'flex'}}>
                     <div style={{display:'flex', marginBottom:'30px'}}>
-                        <input type="text" className="addblogtitle" placeholder="Title" name="title" id='title' required />
+                        <input type="text" className="addblogtitle" placeholder="Title" name="title" id='title' required/>
                     </div>
                     <div style={{display:'flex', marginLeft:'210px'}}>
                         <div className="AttachFile" style={{display:'flex', flexDirection:'column', alignItems:'center', background:"#F2F2F2", borderRadius:'10px', width: "215px", justifyContent: 'center'}}>
